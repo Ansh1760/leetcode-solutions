81ac9d1f27ebf527c4c66efb400eaf287386b3bf
Ashwini Tripathi <130149272+Ansh1760@users.noreply.github.com>
2026-08-18 13:02:19 +0530

    Time: 2 ms (2.50%) | Memory: 47.5 MB (99.63%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,17 +4,17 @@ class Solution {
        int start = 0;
        int end = n-1;
        while(start<=end){
-        int mid = start + (end-start)/2;
-        if(nums[mid]==target){
-            return mid;
+        int mid = end-start/2;
+        if(nums[mid]<target){
+            start = mid+1;
         }
         else if(nums[mid]>target){
             end = mid-1;
         }
         else{
-            start = mid + 1;
+            return mid;
         }
        }
-     return -1;
+       return -1;
     }
 }
